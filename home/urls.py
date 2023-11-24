9de3fcbab5c5f509b7a725f82823f4d5575bee4a
Yunji-Yun <dbswlvkdlxld2022@naver.com>
2023-11-24 17:33:43 +0900

    [1124] 투두 리스트 모델 구축(컴플리트 제외)

--- home/urls.py
+++ home/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('add_list/', views.PostCreate.as_view()),
-
     path('check_details_<int:pk>/', views.PostDetail.as_view()),
     path('', views.PostList.as_view()),
+    path('category/<str:slug>/', views.category_page),
 ]
