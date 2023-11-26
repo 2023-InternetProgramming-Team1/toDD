1b6b14bf01057adb3d2ad5e98128444864cccdcf
Haein Jeong <62838847+tresorhi@users.noreply.github.com>
2023-11-27 00:42:45 +0900

    Update models.py

--- eclass/models.py
+++ eclass/models.py
@@ -1,17 +1,22 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-class Subject(models.Model):
+class Lecture(models.Model):
     name = models.CharField(max_length=100)
+
+class Activity(models.Model):
+    lecture = models.ForeignKey(Lecture, on_delete=models.CASCADE)
+    week = models.IntegerField()
+    title = models.CharField(max_length=50)
 
 class Assignment(models.Model):
     title = models.CharField(max_length=200)
     due_date = models.DateField()
     content = models.TextField()
-    subject = models.ForeignKey(Subject, on_delete=models.CASCADE)
+    activity = models.OneToOneField('Activity', on_delete=models.CASCADE)
 
 class Quiz(models.Model):
     title = models.CharField(max_length=200)
     due_date = models.DateField()
     questions = models.TextField()
-    subject = models.ForeignKey(Subject, on_delete=models.CASCADE)
+    activity = models.OneToOneField('Activity', on_delete=models.CASCADE)
