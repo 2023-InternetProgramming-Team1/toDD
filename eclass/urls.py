422785eaeb472bc1225528de5c171d044ec1014f
tresorhi <tresorhi@naver.com>
2023-11-27 16:14:06 +0900

    [11/27] 이클래스 url 수정

--- eclass/urls.py
+++ eclass/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     path('', views.lecture_list, name='lecture_list'),
     path('lecture/<int:lecture_id>/', views.lecture_detail, name='lecture_detail'),
     path('activity/<int:activity_id>/', views.activity_detail, name='activity_detail'),
-    path('activity/<int:activity_id>/quiz/create/', views.quiz_create, name='quiz_create'),
-    path('activity/<int:activity_id>/assignment/create/', views.assignment_create, name='assignment_create'),
+    path('assignment/create/<int:activity_id>/', views.assignment_creation, name='assignment_create'),
+    path('quiz/create/<int:activity_id>/', views.quiz_creation, name='quiz_create'),
 ]
