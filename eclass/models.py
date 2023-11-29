0690e41240f0d0ae712f8dfcfeb45bb316e74a77
tresorhi <tresorhi@naver.com>
2023-11-29 15:10:11 +0900

    [11/29] 데이터 타입 수정

--- eclass/models.py
+++ eclass/models.py
@@ -11,12 +11,12 @@ class Activity(models.Model):
 
 class Assignment(models.Model):
     title = models.CharField(max_length=200)
-    due_date = models.DateField()
+    due_date = models.DateTimeField()
     content = models.TextField()
     activity = models.OneToOneField('Activity', on_delete=models.CASCADE)
 
 class Quiz(models.Model):
     title = models.CharField(max_length=200)
-    due_date = models.DateField()
+    due_date = models.DateTimeField()
     questions = models.TextField()
     activity = models.OneToOneField('Activity', on_delete=models.CASCADE)
