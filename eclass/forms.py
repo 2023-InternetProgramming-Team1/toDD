bbf536c4a0cd37dff868763d19ff60d99c6eb854
tresorhi <tresorhi@naver.com>
2023-11-29 15:38:48 +0900

    [11/29] urls-템플릿 연결

--- eclass/forms.py
+++ eclass/forms.py
@@ -4,9 +4,37 @@ from .models import Assignment, Quiz
 class AssignmentForm(forms.ModelForm):
     class Meta:
         model = Assignment
-        fields = ['title', 'due_date', 'content']
+        fields = ['title', 'content', 'due_date']
+        widgets = {
+            'title': forms.TextInput(
+                attrs={
+                    'placeholder': '제목 작성',
+                }),
+            'content': forms.Textarea(
+                attrs={
+                    'placeholder': '내용 작성'
+                }),
+            'due_date': forms.DateTimeInput(
+                attrs={
+                    'type': 'datetime-local',
+                }),
+        }
 
 class QuizForm(forms.ModelForm):
     class Meta:
         model = Quiz
         fields = ['title', 'due_date', 'questions']
+        widgets = {
+            'title': forms.TextInput(
+                attrs={
+                    'placeholder': '제목 작성',
+                }),
+            'questions': forms.Textarea(
+                attrs={
+                    'placeholder': '문제 출시'
+                }),
+            'due_date': forms.DateTimeInput(
+                attrs={
+                    'type': 'datetime-local',
+                }),
+        }
