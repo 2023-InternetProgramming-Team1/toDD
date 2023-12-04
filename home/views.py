937963ff6057e61f28d956975051aff13ccd2b34
JSeoEun <cse0514@gmail.com>
2023-12-04 15:39:12 +0900

    [12/04] 마이 내용

--- home/views.py
+++ home/views.py
@@ -151,3 +151,13 @@ def my(request):
         context
     )
 
+def get_weekday_number(day):
+    weekdays = ['월', '화', '수', '목', '금', '토', '일']
+    return weekdays.index(day) + 1
+
+def my_view(request, day):
+    weekday_number = get_weekday_number(day)
+    weekday_posts = Post.objects.filter(complete=True, deadline__week_day=weekday_number)
+
+    return render(request, 'home/my.html', {'weekday_posts': weekday_posts, 'selected_day': day})
+
